89183904b4d6f949773ee34184502ae51ecef3dc
Youssef Gamal <youssef.gamal@intercom.com.eg>
2024-05-16 15:45:10 +0300

    chore: initial spring boot project with required dependencies

---
+++ src/main/java/org/youssef/gamal/file_uploader/app/FileUploaderSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.youssef.gamal.file_uploader.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploaderSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploaderSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/youssef/gamal/file_uploader/app/FileUploaderSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youssef.gamal.file_uploader.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploaderSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
